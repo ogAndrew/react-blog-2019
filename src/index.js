385f727eff332791533b301efbbc4484d798beda
Andrew's Rails Course <talle.andrew@gmail.com>
2019-02-09 22:56:59 -0500

    Approval.js && CommentDetail.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard"; 
 
 const App = () => {
   return (
+    
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+    <ApprovalCard>
+    <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        post="I'm hungry"
+        img={faker.image.avatar()}
+      />
+    </ApprovalCard>
+    <ApprovalCard>
+    <CommentDetail
+        author="Emily"
+        timeAgo="Today at 2:45PM"
+        post="I could have a drink"
+        img={faker.image.avatar()}
+      />
+    </ApprovalCard>
+    <ApprovalCard>
+    <CommentDetail
+        author="Shaun"
+        timeAgo="Yesterday at 7:45PM"
+        post="I love Jesus"
+        img={faker.image.avatar()}
+      />  
+    </ApprovalCard>  
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
